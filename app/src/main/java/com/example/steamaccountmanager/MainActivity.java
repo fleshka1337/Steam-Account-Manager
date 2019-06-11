9dbf1704d2498fbc55ca864598ad833007d77c61
fleshka_1337 <figyshkin2@gmail.com>
2019-06-11 16:36:06 +1000

    new

--- app/src/main/java/com/example/steamaccountmanager/MainActivity.java
+++ app/src/main/java/com/example/steamaccountmanager/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.steamaccountmanager;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
@@ -10,20 +12,36 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
 
+    AccountFragment accountFragment;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    selectedFragment = new AccountFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).
+                            addToBackStack(null).show(selectedFragment)
+                            .detach(selectedFragment).attach(selectedFragment)
+                            .commit();
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_ASF:
+                    selectedFragment = new ASFFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).
+                            addToBackStack(null).show(selectedFragment)
+                            .detach(selectedFragment).attach(selectedFragment)
+                            .commit();
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_account:
+                    selectedFragment = new ProfileFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).
+                            addToBackStack(null).show(selectedFragment)
+                            .detach(selectedFragment).attach(selectedFragment)
+                            .commit();
                     return true;
             }
             return false;
@@ -37,6 +55,16 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        accountFragment = new AccountFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .replace(R.id.fragment_container, accountFragment)
+//                .addToBackStack(null)
+                .show(accountFragment)
+                .commit();
+
     }
 
 }
