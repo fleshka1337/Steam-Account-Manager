5d843f300cc159017acb224dc5ae2e04d1d4ea7d
fleshka_1337 <figyshkin2@gmail.com>
2019-06-15 00:09:27 +1000

    xxxxxsxadasdasd

--- app/src/main/java/com/example/steamaccountmanager/AddAccountSteam.java
+++ app/src/main/java/com/example/steamaccountmanager/AddAccountSteam.java
@@ -8,7 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,10 +18,21 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 /**
@@ -31,11 +44,13 @@ public class AddAccountSteam extends Fragment {
     Button steam_id;
     TextView steam_id_set;
     EditText steam_id_get;
+    ImageView imageView;
 
     String steam;
 
     String steam_id_64;
     String SteamId = null;
+    String background_url = null;
 
     public RequestQueue mQueue, idQueue;
 
@@ -53,6 +68,7 @@ public class AddAccountSteam extends Fragment {
         steam_id = view.findViewById(R.id.steam_id);
         steam_id_set = view.findViewById(R.id.steam_id_set);
         steam_id_get = view.findViewById(R.id.steam_id_get);
+        imageView = view.findViewById(R.id.imageView);
 
         steam = null;
         steam_id_64 = null;
@@ -62,7 +78,7 @@ public class AddAccountSteam extends Fragment {
 
         steam_id.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view){
 //                steam = steam_id_get.getText().toString();
 //                steam_id_set.setText(steam);
                 steam_id_64 = steam_id_get.getText().toString();
@@ -71,6 +87,12 @@ public class AddAccountSteam extends Fragment {
                 }else
                 {
                     parse_steamid_64();
+
+                }
+                try {
+                    getBackgroundURL();
+                } catch (IOException e){
+                    Toast.makeText(getActivity(), "da", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -181,5 +203,54 @@ public class AddAccountSteam extends Fragment {
         mQueue.add(temp_request);
 
     }
+
+    public String getText(String url) throws IOException {
+        URL url2 = new URL(url);
+        HttpsURLConnection connection = (HttpsURLConnection) url2.openConnection();
+        //add headers to the connection, or check the status if desired..
+        connection.setRequestMethod("GET");
+        connection.setConnectTimeout(10);
+        connection.setUseCaches(true);
+        // handle error response code it occurs
+        int responseCode = connection.getResponseCode();
+        InputStream inputStream;
+        if (200 <= responseCode && responseCode <= 299) {
+            inputStream = connection.getInputStream();
+        } else {
+            inputStream = connection.getErrorStream();
+        }
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        inputStream));
+
+        StringBuilder response = new StringBuilder();
+        String currentLine;
+
+        while ((currentLine = in.readLine()) != null)
+            response.append(currentLine);
+
+        in.close();
+
+        return response.toString();
+    }
+
+
+    public void getBackgroundURL() throws IOException {
+        String profile_response = getText("https://steamcommunity.com/id/kojimaone/");
+
+        Pattern p = Pattern.compile("https://steamcdn-a[.]akamaihd[.]net/steamcommunity/public/images/items/[0-9]{6}/[0-9a-z]{40}[.]jpg");
+        // create matcher for pattern p and given string
+        Matcher m = p.matcher(profile_response);
+
+        // if an occurrence if a pattern was found in a given string...
+        if (m.find()) {
+            background_url = m.group(0);
+            Glide.with(getActivity())
+                    .load(background_url)
+                    .into(imageView);
+        }
+    }
+
 
 }
